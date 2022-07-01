36e6753dd0ada124a08b3d3a305fb25ad6d06887
summershin <wsxqaz664422@gmail.com>
2022-07-01 21:17:08 +0800

    加入專案檔案。

---
+++ AJAXLesson/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AJAXLesson.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
